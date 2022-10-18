43a8ca86ba968c1f039c4531df61b60dd753d8dd
Kadir0-7 <101948177+Kadir0-7@users.noreply.github.com>
2022-10-18 19:36:43 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+let removeItem = document.getElementsByClassName("btn-danger")
+
+for(let i = 0; i < removeItem.length; i++){
+    let btn = removeItem[i]
+    btn.addEventListener()
+}
+
+
+
+
+
+
+
+
