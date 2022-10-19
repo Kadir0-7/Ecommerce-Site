26e03becae42d65d562c6b6c2b5d0c9e0794f2d1
Kadir0-7 <101948177+Kadir0-7@users.noreply.github.com>
2022-10-19 19:22:10 -0400

    updated functionality

--- main.js
+++ main.js
@@ -1,13 +1,40 @@
-let removeItem = document.getElementsByClassName("btn-danger")
-
-for(let i = 0; i < removeItem.length; i++){
-    let btn = removeItem[i]
-    btn.addEventListener()
+if(document.readyState == 'loading'){
+    document.addEventListener('DOMContentLoaded', ready)
+}else{
+    ready()
+}
+function ready(){
+    let removeItem = document.getElementsByClassName("btn-danger")
+    for(let i = 0; i < removeItem.length; i++){
+    let btn = removeItem[i];
+    btn.addEventListener('click', (e) => {
+let btnClicks = e.target;
+btnClicks.parentElement.parentElement.remove();
+ UpdateCartPrice();
+    })
+}
 }
 
 
 
 
+
+
+function UpdateCartPrice() {
+
+    let itemContainer = document.getElementsByClassName('cart-items')[0]
+let cartRows = itemContainer.getElementsByClassName('cart-row')
+total = 0
+for(let i = 0; i < cartRows.length; i++){
+    let cartRow = cartRows[i];
+    let priceE = cartRow.getElementsByClassName('cart-price')[0]
+    let quantityE = cartRow.getElementsByClassName('cart-quantity-input')[0]
+let price = parseFloat(priceE.innerText.replace('$', ''));
+let quantity = quantityE.value
+total = total + (price * quantity);
+}
+document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total;
+}
 
 
 
